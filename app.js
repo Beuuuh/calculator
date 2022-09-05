85a644a499a7f4cfaaa93ca8b978aa371361bbe8
Beuuuh <bellmarques8768@hotmail.com>
2022-09-05 17:55:29 -0300

    buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+const BUTTONS = [
+    firstRow = document.querySelector(".frstrow"),
+    secondRow = document.querySelector(".scndrow"),
+    thirdRow = document.querySelector(".thrdrow"),
+    fourthRow = document.querySelector(".frthrow"),
+    fifthRow = document.querySelector(".fithrow")
+];
+
+let label = document.querySelector(".label");
