40f4102a60ed506165b36815402c5e17aed5fa23
Beuuuh <bellmarques8768@hotmail.com>
2022-09-08 18:20:55 -0300

    operator buttons

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ let firstInput = "";
 let operatorInput = "";
 let secondInput = "";
 
+const del = document.querySelector("#delete");
+const dot = document.querySelector("#dot");
+const cleanScreen = document.querySelector("#cleanScreen");
+const operators = [];
 const numbers = [];
+
 numbers.push(document.querySelectorAll([".number"]));
+operators.push(document.querySelectorAll([".operator"]));
+
+operators.forEach((e) => {
+    addEventListener('click', () => {labelChanger(e.innerText)});
+});
 
 numbers.forEach((e) => {
     addEventListener('click', () => {labelChanger(e.innerText)});
 });
 
-function labelChanger(numberButton) {
-    label.textContent += numberButton;
+function labelChanger(Button) {
+    label.textContent += Button;
 };
 
 function add(firstNumber, SecondNumber) {
